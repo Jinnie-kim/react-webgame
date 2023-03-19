420fa12d08e7b5257d8932b4e35d251b54f0ac1e
Jinnie <hjk7639@gmail.com>
2023-03-19 20:46:12 +0900

    반응 속도 미니게임 리셋 버튼 추가

--- reactionVelocityCheck/ReactionCheck.jsx
+++ reactionVelocityCheck/ReactionCheck.jsx
@@ -43,6 +43,12 @@ class ReactionCheck extends Component {
     }
   };
 
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+  };
+
   render() {
     return (
       <>
@@ -50,6 +56,7 @@ class ReactionCheck extends Component {
           {this.state.message}
         </div>
         {this.state.result.length === 0 ? null : <div>평균 시간: {this.state.result.reduce((a, b) => a + b) / this.state.result.length}ms</div>}
+        <button onClick={this.onReset}>reset</button>
       </>
     );
   }
