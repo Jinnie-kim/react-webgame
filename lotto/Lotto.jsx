4b584e950e7ff904d2e5ed6de21080ff858f6814
Jinnie <hjk7639@gmail.com>
2023-03-23 09:36:34 +0900

    로또 추첨기 화면 그리기

--- lotto/Lotto.jsx
+++ lotto/Lotto.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ball from './Ball';
 
-const Lotto = () => {
-  return <h1>hello world</h1>;
-};
+function getWinNumbers() {
+  // 나름 계산량이 많은 함수이기 때문에 반복실행되면 안된다.
+  console.log('getWinNumbers');
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((a, b) => a - b);
+  return [...winNumbers, bonusNumber];
+}
+
+class Lotto extends Component {
+  state = {
+    winNumbers: getWinNumbers(), // 당첨 숫자들
+    winBalls: [],
+    bonus: null, // 보너스 숫자
+    redo: false,
+  };
+
+  render() {
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => (
+            <Ball key={v} number={v} />
+          ))}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        <button onClick={redo ? this.onClickRedo : () => {}}>한 번 더!</button>
+      </>
+    );
+  }
+}
 
 export default Lotto;
