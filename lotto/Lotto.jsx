a3ca6b86bd15cb82b1de12f4e50cad99e9b38777
Jinnie <hjk7639@gmail.com>
2023-03-23 11:09:12 +0900

    오타 수정

--- lotto/Lotto.jsx
+++ lotto/Lotto.jsx
@@ -6,7 +6,7 @@ function getWinNumbers() {
   console.log('getWinNumbers');
   const candidate = Array(45)
     .fill()
-    .map((v, i) => i + 1);
+    .map((_, i) => i + 1);
   const shuffle = [];
   while (candidate.length > 0) {
     shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
@@ -24,6 +24,45 @@ class Lotto extends Component {
     redo: false,
   };
 
+  timeouts = [];
+
+  componentDidMount() {
+    const { winNumbers } = this.state;
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((t) => {
+      clearTimeout(t);
+    });
+  }
+
+  onClickRedo = () => {
+    console.log('onclickRedo');
+    this.setState({
+      winNumbers: getWinNumbers(), // 당첨 숫자들
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    });
+    this.timeouts = [];
+    // state 초기화 후 setTimeout도 다시 해줘야함.
+  };
+
   render() {
     const { winBalls, bonus, redo } = this.state;
     return (
@@ -36,7 +75,7 @@ class Lotto extends Component {
         </div>
         <div>보너스!</div>
         {bonus && <Ball number={bonus} />}
-        <button onClick={redo ? this.onClickRedo : () => {}}>한 번 더!</button>
+        {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
       </>
     );
   }
