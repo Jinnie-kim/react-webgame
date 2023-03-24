09a4040a95288b49db4ac1b90777dfe81e634e46
Jinnie <hjk7639@gmail.com>
2023-03-24 09:45:48 +0900

    틱택토 - table과 그 하위 요소 컴포넌트 생성, useReducer 사용

--- TicTacToe/Tictactoe.jsx
+++ TicTacToe/Tictactoe.jsx
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useReducer } from 'react';
+import Table from './Table';
 
-class Tictactoe extends Component {
-  render() {
-    return <h1>hi</h1>;
-  }
-}
+const initialState = {
+  winner: '',
+  turn: 'O',
+  tableData: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ],
+};
+
+const Tictactoe = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const [winner, setWinner] = useState('');
+  // const [turn, setTurn] = useState('O');
+  // const [tableData, setTableData] = useState([
+  //   ['', '', ''],
+  //   ['', '', ''],
+  //   ['', '', ''],
+  // ]);
+  return (
+    <>
+      <Table />
+      {winner && <div>{winner}님의 승리</div>}
+    </>
+  );
+};
 
 export default Tictactoe;
